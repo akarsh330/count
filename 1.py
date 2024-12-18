a4cd843a802411587acd6e2df6cda7b80b1cce5e
akarsh330 <akarshgowda7711@gmail.com>
2024-12-18 09:02:02 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,15 @@
+count=0
+val='arjun'
+for x in val:
+    print(x)
+    count=count+1
+    print(count)
+
+
+
+
+    count=0
+    for x in range(1,10,2):
+        print(x)
+        count=count+1
+    print('total values of odd',count)
